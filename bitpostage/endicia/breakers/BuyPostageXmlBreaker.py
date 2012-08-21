5a9188d37693add0249e7b64fb17b38ed8b4fba0
elchupa <lol@lol.com>
2012-08-20 23:23:17 -0400

    Added another test and started to keep track of code coverage of the unit tests. Current is 96%, goal is 100% coverage

--- bitpostage/endicia/breakers/BuyPostageXmlBreaker.py
+++ bitpostage/endicia/breakers/BuyPostageXmlBreaker.py
@@ -13,11 +13,28 @@ class BuyPostageXmlBreaker( EndiciaXmlBreaker ):
 		tree = etree.fromstring( self.xml )
 
 		_map["Status"] = tree.findtext( "Status" )
+		if _map["Status"] == None:
+			raise MissingValueXmlError( "Status" )
+
 		_map["RequesterID"] = tree.findtext( "RequesterID" )
+		if _map["RequesterID"] == None:
+			raise MissingValueXmlError( "RequesterID" )
+
 		_map["RequestID"] = tree.findtext( "RequestID" )
+		if _map["RequestID"] == None:
+			raise MissingValueXmlError( "RequestID" )
+
 		_map["AccountID"] = tree.findtext( "./CertifiedIntermediary/AccountID" )
+		if _map["AccountID"] == None:
+			raise MissingValueXmlError( "AccountID" )
+
 		_map["PostageBalance"] = tree.findtext( "./CertifiedIntermediary/PostageBalance" )
+		if _map["PostageBalance"] == None: 
+			raise MissingValueXmlError( "PostageBalance" )
+
 		_map["AscendingBalance"] = tree.findtext( "./CertifiedIntermediary/AscendingBalance" )
+		if _map["AscendingBalance"] == None:
+			raise MissingValueXmlError( "AscendingBalance" )
 
 		if tree.find( "ErrorMessage" ) != None:
 			_map["ErrorMessage"] = tree.findtext( "ErrorMessage" )
